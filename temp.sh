e386989f5572a63ecf804f7703e37bfb8f558014
Lars Kiesow <lkiesow@uos.de>
2021-02-13 16:57:11 +0100

    Indent using 2 spaces

--- temp.sh
+++ temp.sh
@@ -9,15 +9,15 @@ S3_LOCATION="sensors/temperature-server/metrics"
 SLEEP_SECONDS=60
 
 if [ ! -f "${S3CFG_FILE}" ]; then
-	echo "Error: Configuration for s3cmd is missing."
-	echo "       Expecting it to be at ${S3CFG_FILE}."
-	exit 1
+  echo "Error: Configuration for s3cmd is missing."
+  echo "       Expecting it to be at ${S3CFG_FILE}."
+  exit 1
 fi
 
 while true; do
-	echo '# HELP temperature Temperature from sensor' > "${TEMPERATURE_FILE}.tmp"
-	echo '# TYPE temperature gauge' >> "${TEMPERATURE_FILE}.tmp"
-	temper-poll | sed -n 's/Device.*: *\(.*\)°C *\(.*\)°F.*$/temperature{unit="C",} \1\ntemperature{unit="F",} \2/p' >> "${TEMPERATURE_FILE}.tmp"
+  echo '# HELP temperature Temperature from sensor' > "${TEMPERATURE_FILE}.tmp"
+  echo '# TYPE temperature gauge' >> "${TEMPERATURE_FILE}.tmp"
+  temper-poll | sed -n 's/Device.*: *\(.*\)°C *\(.*\)°F.*$/temperature{unit="C",} \1\ntemperature{unit="F",} \2/p' >> "${TEMPERATURE_FILE}.tmp"
 
   if diff -q "${TEMPERATURE_FILE}" "${TEMPERATURE_FILE}.tmp"; then
     rm "${TEMPERATURE_FILE}.tmp"
@@ -26,5 +26,5 @@ while true; do
     s3cmd -c "${S3CFG_FILE}" put "${TEMPERATURE_FILE}" "s3://${S3_LOCATION}"
   fi
 
-	sleep $SLEEP_SECONDS
+  sleep $SLEEP_SECONDS
 done
